14ce107edafe7adc061cf250733dbbeb7a933c5c
Fthi-S3074641 <fthialem@gmail.com>
2019-07-03 10:55:17 +0300

    =in blocks netlify custom domain

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#037367',
+    secondary: '#00281f',
+    accent: '#4a7eb3',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})

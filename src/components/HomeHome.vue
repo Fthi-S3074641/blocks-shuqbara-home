e592832c5defb6f307d832bf958a67f08f5e1888
Fthi-S3074641 <fthialem@gmail.com>
2019-08-14 10:55:48 +0300

    =inserted logo

--- src/components/HomeHome.vue
+++ src/components/HomeHome.vue
@@ -7,7 +7,7 @@
             justify-center
             class="white--text"
           >
-          <v-img :src="require('@/assets/logo.png')"/> 
+          <v-img :src="'https://raw.githubusercontent.com/Fthi-S3074641/aksumitetours/master/dist/l.png'" /> 
             <h1 class="white--text mb-2 display-1 text-center">Shuqbara Foundations</h1>
             <div class="subheading mb-4 text-center">Mobile apps | Web design | CGI products </div>
             <v-btn fab class="mt-5 brown darken-4">
